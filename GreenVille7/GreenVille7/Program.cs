d4fa348cb635b4999750f4a92e7c528225e5fba7
Bucket Head Wendy <Bucket Head Wendy@LAPTOP-VDJKUD45>
2017-03-22 17:34:52 -0600

    Added methods to record this year and last year's number of contestants, and added method writing the appropriate statement based on the totals

---
+++ GreenVille7/GreenVille7/Program.cs
@@ -0,0 +1,141 @@
+﻿//Program written by Dan Plummer and Derek West
+//1/.20.17
+//CIS 2620 | Chapter 2 | Case Study #1
+
+//2/15/17
+//CIS 2620 | Chapter 4 | Case Study #1
+
+//2/26/17
+//CIS 2620 | Chapter 5 | Case Study #1
+
+//3/15/2017 
+//CIS 2620 | Chapter 6 | Case Study #1
+
+//3/22/2017
+//CIS 2620 | Chapter 7 | Case Study #1
+
+using System;
+using static System.Console;
+
+class GreenvilleRevenue
+{
+    static void Main()
+    {
+        int lastYearNum, thisYearNum;
+        string lastYear, thisYear;
+        double admission = 25.00, revenue;
+
+        Write("For last year, ");
+        lastYearNum = Contestants();
+        Write("For this year, ");
+        thisYearNum = Contestants();
+
+
+        while (lastYearNum < 0 || lastYearNum > 30)
+        {
+            Write("Invalid entry, please enter a number from 0 to 30: ");
+            Contestants();
+        }
+
+        bool moreThisYear = thisYearNum > lastYearNum;
+        revenue = thisYearNum * admission;
+
+        while (thisYearNum < 0 || thisYearNum > 30)
+        {
+            Write("Invalid entry, please enter a number from 0 to 30: ");
+            thisYear = ReadLine();
+            thisYearNum = Int32.Parse(thisYear);
+        }
+
+        string[] contestantTalents = { "S", "D", "M", "O" };
+        string[] currentNames = { };
+        string[] currentTalents;
+        int[] talentCounts = { 0, 0, 0, 0 };
+        currentNames = new string[thisYearNum];
+        string inputSkill, inputName;
+        bool isValid = false;
+        int x = 0;
+        int y = 0;
+
+        while (x < currentNames.Length)
+
+        {
+            Write("Please enter the name for Contestant #{0}: ", x + 1);
+            inputName = ReadLine();
+            Write("Please enter a skill, either S for singing, D for Dancing, M for Musical Intrument, or O for Other: ");
+            inputSkill = ReadLine();
+
+            while (isValid == false)
+            {
+                for (y = 0; y < contestantTalents.Length; y++)
+                {
+                    if (inputSkill == contestantTalents[y])
+                    {
+                        isValid = true;
+                        talentCounts[y] += 1;
+                    }
+                }
+                if (isValid == false)
+                {
+                    WriteLine("The skill you have entered is invalid.");
+                    Write("Please enter a skill, either S for singing, D for Dancing, M for Musical Intrument, or O for Other: ");
+                    inputSkill = ReadLine();
+                }
+
+            }
+
+            currentNames[x] = Convert.ToString(inputName);
+            x += 1;
+            isValid = false;
+
+
+        }
+
+        inputSkill = "";
+        while (inputSkill != "Q")
+        {
+            WriteLine("Enter a talent code to see how many contestants will be showcasing that talent this year OR enter Q to quit: ");
+            inputSkill = ReadLine();
+            for (y = 0; y < contestantTalents.Length; y++)
+            {
+                if (inputSkill == contestantTalents[y])
+                {
+                    WriteLine("{0} contestant(s) will be showcasing that talent this year.", talentCounts[y]);
+                }
+            }
+
+
+        }
+
+        //Chapter 7 addition
+        RaceMessage(thisYearNum, lastYearNum);
+        WriteLine();
+        WriteLine("Expected revenue for this year's competition is: ${0}", revenue.ToString("F2"));
+    }
+
+    //Chapter 7 addition begin
+
+    private static int Contestants()
+    {
+        string userInput;
+        int numContestants;
+
+        WriteLine("please enter the number of contestants: ");
+        userInput = ReadLine();
+        numContestants = Convert.ToInt32(userInput);
+        return numContestants;
+    }
+    private static void RaceMessage(int valThisYear, int valLastYear)
+    {
+
+        if (valThisYear > (valLastYear * 2))
+            WriteLine("The Competition is more than twice as big this year!");
+        else
+               if (valThisYear > valLastYear)
+            WriteLine("The competition is bigger than ever!");
+        else
+               if (valThisYear < valLastYear)
+            WriteLine("A tighter race this year! Come out and cast your vote.");
+    }
+    //Chapter 7 additon end
+}
